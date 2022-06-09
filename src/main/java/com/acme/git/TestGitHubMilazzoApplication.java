04f0dba59a315d486e55bf33ebaef4da6a52c5d0
rosam <rosam@LAPTOP-J9NR809C>
2022-06-09 16:28:14 +0200

    Creazione

---
+++ src/main/java/com/acme/git/TestGitHubMilazzoApplication.java
@@ -0,0 +1,13 @@
+package com.acme.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitHubMilazzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitHubMilazzoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/git/TestGitHubMilazzoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitHubMilazzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
